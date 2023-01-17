35bd0af1c5a323987e9520b73b498eeb7a74d297
jorgegoco <jorgegoco70@gmail.com>
2023-01-17 14:46:19 +0100

    Add some routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
@@ -72,11 +75,24 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        <Routes>
+          <Route
+            path="/"
+            element={(
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
+              </>
+            )}
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import PropTypes, { bool } from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import Button from './Button';
 
-const Header = ({ title, onAdd, showAdd }) => (
-  <header className="header">
-    <h1>{title}</h1>
-    <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
 
-  </header>
-);
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      {location.pathname === '/' && <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />}
+
+    </header>
+  );
+};
 
 Header.defaultProps = {
   title: 'Task Tracker',
